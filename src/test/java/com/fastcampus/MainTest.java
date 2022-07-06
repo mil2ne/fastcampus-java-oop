2753f9795d43eb1077201f9f75a18b7133f1e794
celee <show3070@gmail.com>
2022-07-06 13:55:21 +0900

    11111

--- src/test/java/com/fastcampus/MainTest.java
+++ src/test/java/com/fastcampus/MainTest.java
@@ -9,7 +9,7 @@ class MainTest {
     @Test
     void main() {
         // given
-        String[] args = {"7","4","3","2", "1"};
+        String[] args = {"9","4","3","2", "1"};
 
         // when & then
         Main.main(args);
